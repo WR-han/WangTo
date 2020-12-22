ebce2251aab71ce8890e72035cca1b77fd332831
wrhan <wr.han@foxmail.com>
2020-12-22 18:02:05 +0800

    解压上传

--- src/views/account/account.vue
+++ src/views/account/account.vue
@@ -3,7 +3,7 @@
     <el-upload
       class="upload-demo"
       ref="upload"
-      action="https://jsonplaceholder.typicode.com/posts/"
+      action=""
       :on-preview="handlePreview"
       :on-remove="handleRemove"
       :file-list="fileList"
@@ -13,6 +13,7 @@
       :before-upload="sb4"
       :on-change="sb5"
       :auto-upload="false"
+      :http-request="sb6"
     >
       <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
       <el-button
@@ -30,30 +31,32 @@
 </template>
 
 <script>
-import JsZip from "jszip";
+import JSZip from "jszip";
+import axios from "axios";
 export default {
   name: "account",
   components: {},
   props: {},
   data() {
     return {
-      fileList: [
-        {
-          name: "food.jpeg",
-          url:
-            "https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100",
-        },
-        {
-          name: "food2.jpeg",
-          url:
-            "https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100",
-        },
-      ],
+      fileList: [],
+      abc: 123,
     };
   },
   methods: {
     submitUpload() {
-      this.$refs.upload.submit();
+      console.log(this.fileList);
+
+      let formData = new FormData();
+      this.fileList.forEach((item) => {
+        console.log(item)
+        formData.append("files", item);
+      });
+
+      // console.log(formData)
+
+      axios.post("http://127.0.0.1:8000/test", formData);
+      // this.$refs.upload.submit();
     },
     handleRemove(file, fileList) {
       console.log("文件列表移除文件时的钩子", file, fileList);
@@ -70,12 +73,49 @@ export default {
     sb3(file) {
       console.log("sb的钩子", file);
     },
-    sb4(file){
-      console.log("before-upload",file);
+    sb4(file) {
+      console.log("before-upload", file);
     },
-    sb5(file, fileList){
-      console.log("on-change",file, fileList);
-    }
+    sb5(file, fileList) {
+      this.handleFile(file.raw);
+      // console.log("on-change", file);
+      console.log(this.$refs);
+    },
+    sb6() {
+      console.log("123456");
+    },
+    handleFile(f) {
+      let vue_obj = this;
+      JSZip.loadAsync(f).then(
+        function (zip) {
+          // ---------- 文件总列表 （筛出目录 以/结尾）
+          let file_name_list = Object.keys(zip.files).filter((name) => {
+            return name.charAt(name.length - 1) != "/";
+          });
+          // ---------- 文件总数
+          let all_file_num = file_name_list.length;
+          // ---------- 初始解压数
+          let is_zip = 1;
+          // ---------- 便利解压后的文件
+          zip.forEach(function (relativePath, zipEntry) {
+            // 2) print entries
+            // ---------- 当前文件大小
+            let file_size = zipEntry._data.uncompressedSize;
+            // ---------- 判断文件大小是否为0
+            if (file_size) {
+              zipEntry.async("blob").then((file) => {
+                let new_file = new File([file], relativePath);
+                vue_obj.fileList.push(new_file);
+                // vue_obj.$refs.upload.fileList.push(new_file);
+              });
+            }
+          });
+        },
+        function (e) {
+          console.log("error", e);
+        }
+      );
+    },
   },
   watch: {},
   computed: {},
